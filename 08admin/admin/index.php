346300ffbee536a687c0758ab0453ef5a4093b57
Trong Nghia <trongnghia77@live.com>
2023-01-23 10:35:54 +0700

    65-admin-table

--- 08admin/admin/index.php
+++ 08admin/admin/index.php
@@ -1,6 +1,8 @@
 <?php
 require '../includes/init.php';
 
+Auth::requireLogin();
+
 $conn = require '../includes/db.php';
 
 $articles = Article::getAll($conn);
@@ -18,19 +20,27 @@ if (Auth::isLoggedIn()):
     <p>You are not logged in. <a href="login.php">Login</a></p>
 <?php endif; ?>
 
+<h2>Admin page</h2>
+
 <?php if (empty($articles)): ?>
     <p>No articles found.</p>
 <?php else: ?>
-    <ul>
-        <?php foreach ($articles as $article): ?>
-            <li>
-                <article>
-                    <h2><a href="article.php?id=<?= htmlspecialchars($article["Id"]); ?>"><?= $article['Title'] ?></a></h2>
-                    <p><?= htmlspecialchars($article['Content']); ?></p>
-                </article>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    
+    <table>
+        <thead>
+            <th>Title</th>
+        </thead>
+        <tbody>
+            <?php foreach ($articles as $article): ?>
+                <tr>
+                    <td>
+                        <a href="article.php?id=<?= htmlspecialchars($article["Id"]); ?>"><?= $article['Title'] ?></a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
 <?php endif; ?>
         
 <?php require '../includes/footer.php'; ?>
