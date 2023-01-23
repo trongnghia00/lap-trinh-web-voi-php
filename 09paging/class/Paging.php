53b46634f22b9aef1f8f8987455576468991311f
Trong Nghia <trongnghia77@live.com>
2023-01-23 20:50:27 +0700

    69-paging-validate-page

--- 09paging/class/Paging.php
+++ 09paging/class/Paging.php
@@ -13,6 +13,14 @@ class Paging
     public function __construct($page, $articles_per_page)
     {
         $this->limit = $articles_per_page;
+
+        $page = filter_var($page, FILTER_VALIDATE_INT, [
+            'options' => [
+                'default' => 1,
+                'min_range' => 1
+            ]
+        ]);
+
         $this->offset = $articles_per_page * ($page - 1);
     }
 }
