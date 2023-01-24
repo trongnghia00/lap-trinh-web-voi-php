b22f3d5375f1c1f0810a05855340cde23ded14ff
Trong Nghia <trongnghia77@live.com>
2023-01-24 21:27:03 +0700

    74-upload-error-handle

--- 10upload/admin/edit_article_image.php
+++ 10upload/admin/edit_article_image.php
@@ -16,6 +16,21 @@ if (isset($_GET['id'])) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     var_dump($_FILES);
+
+    try {
+        switch ($_FILES['file']['error']) {
+            case UPLOAD_ERR_OK :
+                break;
+            case UPLOAD_ERR_NO_FILE :
+                throw new Exception('No file uploaded.');
+                break;
+            default :
+                throw new Exception('An error occurred.');
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    
 }
 
 ?>
